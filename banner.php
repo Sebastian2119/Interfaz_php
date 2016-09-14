8b9df0735b3d500fd0f9dfa0bbdc30290cccce2e
Sebastian2119 <bscastillo26@misena.edu.co>
2016-09-14 15:37:14 -0500

    Index_Banner

---
+++ banner.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Documento sin t&iacute;tulo</title>
+<style type="text/css">
+<!--
+.Estilo10 {color: #000000; }
+-->
+</style>
+</head>
+
+<body>
+<table width="200" border="0">
+  <tr>
+    <td><img src="Imagen1.png" name="banner" width="1496" height="273" id="banner" /></td>
+  </tr>
+</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Documento sin t&iacute;tulo</title>
+<style type="text/css">
+<!--
+.Estilo1 {font-family: Arial, Helvetica, sans-serif}
+.Estilo8 {font-family: Arial, Helvetica, sans-serif; color: #000000; }
+.Estilo10 {color: #000000; }
+.Estilo14 {font-size: x-large; }
+-->
+</style>
+</head>
+
+<body>
+<div align="center">
+  <table width="1310" height="691" border="0">
+    <tr>
+      <td height="206" colspan="4">
+	  <?php
+	  include('banner.php');
+	  ?>	  </td>
+    </tr>
+    <tr>
+      <td width="270" height="50" bordercolor="#333333" bgcolor="#FFFFFF" class="Estilo1"><p class="Estilo14"><span class="Estilo8">Venta</span></p></td>
+      <td width="262" bordercolor="#333333" bgcolor="#FFFFFF" class="Estilo1"><p class="Estilo14"><span class="Estilo1">Arriendo</span></p></td>
+      <td bordercolor="#333333" bgcolor="#FFFFFF" class="Estilo1"><p class="Estilo14">Venta fuera de bogota</p></td>
+      <td bgcolor="#FFFFFF" class="Estilo8"><form id="form2" name="form2" method="post" action="">
+        <label>Buscar
+          <input name="buscar" type="text" id="buscar" />
+          </label>
+      </form>      </td>
+    </tr>
+    <tr>
+      <td height="317" colspan="2"><img src="iniciar sesion.png" width="538" height="331" /></td>
+      <td colspan="2" bgcolor="#CCFFFF"><p class="Estilo1">Escribe tu nombre y contrase&ntilde;a para ingresar</p>
+        <form id="form1" name="form1" method="post" action="negocio.php">
+          <span class="Estilo1">Escriba tu nombre:
+              <label>
+            <input name="nombre" type="text" id="nombre" />
+            </label>
+          </span>
+            <p><span class="Estilo1">Escriba tu contrase&ntilde;a</span>:
+              <label>
+                <input name="contrasena" type="password" id="contrasena" />
+              </label>
+            </p>
+            <p>
+              <label>
+              <input name="Ingresar" type="submit" class="Estilo1" id="Ingresar" value="Ingresar" />
+              </label>
+            </p>
+          <p class="Estilo1"><a href="Restablecer contrase&ntilde;a.php">Olvide mi contrase&ntilde;a</a></p>
+        </form></td>
+    </tr>
+    <tr>
+      <td colspan="2" bgcolor="#FFFFFF"><span class="Estilo8">Sigenos</span><span class="Estilo10">: <img src="facebook.png" width="23" height="28" align="absmiddle" /> <img src="Nuevo-CEO-de-Twitter.png" width="23" height="27" align="absmiddle" /></span></td>
+      <td width="446" bgcolor="#FFFFFF"><div align="left">
+          <p class="Estilo1">Smart Property:<img src="474487-icono-ubicacion.png" width="32" height="35" align="absmiddle" />Carrera 12 D BIS No. 24 - 91 Sur, Bogota D.C.</p>
+          <p class="Estilo1">Mandanos un E-mail: smarpropertysiw@gmail.com</p>
+      </div></td>
+      <td width="314" bgcolor="#FFFFFF"><span class="Estilo1"><img src="logo telefono.png" width="28" height="30" align="absmiddle" />Llamanos: (+57) 3132440125</span></td>
+    </tr>
+  </table>
+</div>
+</body>
+</html>
